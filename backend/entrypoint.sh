da1db75ae3d76605e5b285f72ed26cb1db6a559d
tetelevm <tetelev99@mail.ru>
2023-12-09 03:20:17 +0300

    Small Docker improvements

--- backend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-poetry run python3 manage.py migrate --no-input
-poetry run python3 manage.py collectstatic --no-input
-poetry run gunicorn server.asgi:application --config gunicorn_conf.py
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+service_urls = [
+    path("adminpanel/", admin.site.urls),
+    path("select2/", include("django_select2.urls")),
+]
+
+
 urlpatterns = [
     path("api/", include([])),
-    path('adminpanel/', admin.site.urls),
-    path("select2/", include("django_select2.urls")),
+    path("service/", include(service_urls)),
 ]
