0ef9e9200d61bc917158d483e4b6fd70ad76110d
tetelevm <tetelev99@mail.ru>
2023-12-11 01:19:19 +0300

    Configured volumes in docker

--- backend/gunicorn_conf.py
+++ backend/gunicorn_conf.py
@@ -1,7 +1,10 @@
+import os
+
 workers = 4
 max_requests = 1000
 timeout = 30
 preload_app = True
 worker_class = "uvicorn.workers.UvicornWorker"
 
+reload = os.getenv("DEBUG", "0") in ["1", "True", "true"]
 bind = "0.0.0.0:8000"
